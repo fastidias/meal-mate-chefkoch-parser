0976391238b8e96b468558a2e0ec13a70fdc56f7
Philipp Wilke <philipp.wilke@stud.leuphana.de>
2023-06-29 22:16:51 +0200

    extent recipe by title and fix ingredient parser issues

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # meal-mate-chefkoch-parser
 pip install beautifulsoup4
+
+# TODOs:
+- add unit in ingredients
+- group ingredients instead of grouping in one list (example: https://www.chefkoch.de/rezepte/2305771367945878/Elisenlebkuchen.html )
--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 import urllib.request
 import ssl
 from bs4 import BeautifulSoup
+import json
+import time
 
+
+def parse_rezept(url):
+    context = ssl._create_unverified_context()
+    request = urllib.request.urlopen(url, context=context)
+    response = request.read()
+
+    rezeptHtml = response.decode("utf8")
+    request.close()
+
+    rezeptHtml = BeautifulSoup(rezeptHtml, "html.parser")
+
+    try:
+        return {
+            "title": parse_title(rezeptHtml),
+            "ingredients": parse_ingredients(rezeptHtml),
+            "source": request.geturl()
+        }
+    except NameError:
+        print(request.geturl())
+
+    return {}
 
 def parse_ingredients(html):
-    trs = html.body.find_all('tr')  # TODO: specify tr of table.ingredients
+    tbodies = html.body.find_all('tbody')  # TODO: specify tr of table.ingredients
+    ingredients = []
 
-    for child in trs:
-        amount = child.find('td', attrs={'class': 'td-left'}).text.strip().replace(" ", "")
-        ingredient = child.find('td', attrs={'class': 'td-right'}).text.strip()
-        print(amount + " " + ingredient)
+    for tbody in tbodies:
+        trs = tbody.find_all('tr')
+        for child in trs:
+            amount = child.find('td', attrs={'class': 'td-left'}).text.strip().replace(" ", "")
+            name = child.find('td', attrs={'class': 'td-right'}).text.strip()
+            ingredients.append({"name": name, "quantity": amount, "unit": ""})
 
+    return ingredients
 
-# url = "https://www.chefkoch.de/rezepte/zufallsrezept/"
-url = "https://www.chefkoch.de/rezepte/2076041335467470/Hackmuffins.html"
-context = ssl._create_unverified_context()
-request = urllib.request.urlopen(url, context=context)
-response = request.read()
+def parse_title(html):
+    title = html.body.find_all('h1')
+    return title[0].contents[0]
 
-rezeptHtml = response.decode("utf8")
-request.close()
+url = "https://www.chefkoch.de/rezepte/zufallsrezept/"
 
-rezeptHtml = BeautifulSoup(rezeptHtml, "html.parser")
-parse_ingredients(rezeptHtml)
+rezepte = []
+for i in range(10):
+    time.sleep(0.5)
+    rezepte.append(parse_rezept(url))
+
+with open("rezepte.json", "w", encoding='utf-8') as text_file:
+    text_file.write(json.dumps(rezepte, ensure_ascii=False, indent=4))
